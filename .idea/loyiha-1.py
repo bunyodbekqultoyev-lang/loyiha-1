7b2b917723d999c8ce3cf8408ecfab8dd1154eb0
bunyodbekqultoyev-lang <bunyodbekqultoyev@gmail.com>
2026-07-08 10:35:39 +0500

    Add loiyiha-1.py with basic math operations

---
+++ .idea/loyiha-1.py
@@ -0,0 +1,16 @@
+
+print(""" "Nexia", "Tico", 'Damas' ko'rganlar qilar havas """)
+print("-" * 40)
+
+
+print(f"5 ning 4-darajasi {5 ** 4}")
+
+
+print(f"22 ni 4 ga bo'lganda qoldiq {22 % 4} qoladi")
+
+tomon = 125
+print(f"Tomonlari {tomon} ga teng kvadratning yuzi {tomon ** 2} va perimetri {4 * tomon}")
+
+diametr = 12
+radius = diametr / 2
+print(f"Diametri {diametr} ga teng bo'lgan doiraning yuzi {3.14 * (radius ** 2)}")
